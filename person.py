49f0d7ceecf0992ef5434443d69f1d137eb5edc8
Pawan Kumar <kumar16.pawan@gmail.com>
2019-03-02 21:55:57 +0530

    new function addded

--- person.py
+++ person.py
@@ -1,3 +1,4 @@
+# import classes
 class Person:
     def __init__(self,name,job=None,pay=0):
         self.name=name
@@ -5,10 +6,16 @@ class Person:
         self.pay=pay  
     def display(self):
         print(self.name,',you work as',self.job,' and you earn ',self.pay)
-
-        
-Pawan=Person('Pawan')
-Lali=Person('Lalit Kumar','Backend Devleoper')
-# x.name="Pawan"
+    def increment(self,percent):
+        self.pay=self.pay+self.pay*percent/100
+        # +(percent/100)*self.pay
+# if __name__=="__main__":
+Pawan=Person('Pawan Kumar','',1200)
+Lali=Person('Lalit Kumar','Backend Devleoper',1000)
+# Lali=Person('Lali Kumar','Backend Devleoper',1000)
+Lali.increment(20)
+Pawan.increment(30)
+    # x.name="Pawan"
 Pawan.display()
+Lali.display()
 Lali.display()
