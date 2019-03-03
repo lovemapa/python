09d22439d32d955477b4eb28fccc7fc710c7d5e2
Pawan Kumar <kumar16.pawan@gmail.com>
2019-03-03 18:37:11 +0530

    added a manager class

--- person.py
+++ person.py
@@ -7,15 +7,23 @@ class Person:
     def display(self):
         print(self.name,',you work as',self.job,' and you earn ',self.pay)
     def increment(self,percent):
-        self.pay=self.pay+self.pay*percent/100
+        self.pay=int(self.pay+self.pay*percent/100)
         # +(percent/100)*self.pay
+
+class manager(Person):
+    def increment(self,percent,bonus=10):
+        Person.increment(self,percent+bonus)
+    def  display(self):
+        Person.display(self)
+    def perks(self,appraisel):
+        self.pay=self.pay+appraisel
 # if __name__=="__main__":
-Pawan=Person('Pawan Kumar','',1200)
-Lali=Person('Lalit Kumar','Backend Devleoper',1000)
-# Lali=Person('Lali Kumar','Backend Devleoper',1000)
-Lali.increment(20)
-Pawan.increment(30)
+Pawan=Person('Pawan Kumar','Back End developer',1000)
+Anoop=manager('Anoop Kumar','Manager',1000)
+
+Pawan.increment(10)
+Anoop.increment(10)
+# Anoop.perks(1000)
     # x.name="Pawan"
 Pawan.display()
-Lali.display()
-Lali.display()
+Anoop.display()
